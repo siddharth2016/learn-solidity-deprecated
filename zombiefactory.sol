e26cc318c2d87ef8597e6407663abab754985c5b
siddharth2016 <siddharthchandragzb@gmail.com>
2022-04-18 16:07:58 +0530

    inherit Ownable contract

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,14 +1,19 @@
 pragma solidity >=0.5.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
     event NewZombie(uint id, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -17,7 +22,7 @@ contract ZombieFactory {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime)));
         zombieToOwner[zombies.length - 1] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(zombies.length - 1, _name, _dna);
