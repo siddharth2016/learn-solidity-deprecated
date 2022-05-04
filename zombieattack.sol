35cfccde90ac1a2f510ebd8d8c835d2bfac9ea8a
siddharth2016 <siddharthchandragzb@gmail.com>
2022-05-04 18:25:44 +0530

    create randNumber generator

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,5 +3,17 @@ pragma solidity >=0.5.0;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-    
+
+    uint randNonce = 0;
+
+    uint attackVictoryProbability = 70;
+
+    function randNumber(uint _modulus) internal returns (uint) {
+        randNonce++;
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce)));
+    }
+
+    function attack(uint _zombieId, uint _targetId) external {
+        
+    }
 }
